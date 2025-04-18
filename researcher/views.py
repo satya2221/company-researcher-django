ce7aa57f478444ce28d724093fa94dcc9e1ab31d
satya2221 <satyaghifari12@gmail.com>
2025-04-18 07:28:14 +0700

    get user input & make queries from the input

--- researcher/views.py
+++ researcher/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .methods import generate_queries
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
+        return render(request, "index.html")
+    
+    def post(self, request):
+        company_name = request.POST.get("company_name")
+
+        financial_queries = generate_queries("financial",company_name)
+        leadership_queries = generate_queries("leadership",company_name)
+        operations_queries = generate_queries("operations",company_name)
+        corporate_history_queries = generate_queries("corporate history",company_name)
+
+        print(f"Financial: {financial_queries}")
+        print(f"Leadership: {leadership_queries}")
+        print(f"Operations: {operations_queries}")
+        print(f"Corporate History: {corporate_history_queries}")
+
         return render(request, "index.html")

445d1d3cb8cc0476013a35284f571c9d767f6e08
satya2221 <satyaghifari12@gmail.com>
2025-04-19 20:42:09 +0700

    Finalize websocket notification of each process

--- researcher/tasks.py
+++ researcher/tasks.py
@@ -7,31 +7,31 @@ import time
 def process_research(company_name):
     queries = []
     
-    time.sleep(5)
-    send_notification("test", "test notification 1")
+    send_notification("notification", "Generating Financial Queries")
+    financial_queries = generate_queries("financial",company_name)
 
-    time.sleep(5)
-    send_notification("test", "test notification 2")
+    send_notification("notification", "Generating Leadership Queries")
+    leadership_queries = generate_queries("leadership",company_name)
 
-    time.sleep(5)
-    send_notification("test", "test notification 3")
-    # financial_queries = generate_queries("financial",company_name)
-    # leadership_queries = generate_queries("leadership",company_name)
-    # operations_queries = generate_queries("operations",company_name)
-    # corporate_history_queries = generate_queries("corporate history",company_name)
+    send_notification("notification", "Generating Operations Queries")
+    operations_queries = generate_queries("operations",company_name)
 
-    # queries.extend(financial_queries.get("queries"))
-    # queries.extend(leadership_queries.get("queries"))
-    # queries.extend(operations_queries.get("queries"))
-    # queries.extend(corporate_history_queries.get("queries"))
+    send_notification("notification", "Generating Corporate History Queries")
+    corporate_history_queries = generate_queries("corporate history",company_name)
 
-    # context = ""
+    queries.extend(financial_queries.get("queries"))
+    queries.extend(leadership_queries.get("queries"))
+    queries.extend(operations_queries.get("queries"))
+    queries.extend(corporate_history_queries.get("queries"))
 
-    # for query in queries:
-    #     response = research(query)
+    context = ""
 
-    #     context += f"Query: {query} \nResponse: {response}\n\n"
+    for query in queries:
+        send_notification("notification", f"Researching about {query}")
+        response = research(query)
+        context += f"Query: {query} \nResponse: {response}\n\n"
 
-    # business_profile = generate_business_profile(context)
+    send_notification("notification", "Generating Business Profile")
+    business_profile = generate_business_profile(context)
 
-    # print(business_profile)
+    send_notification("final_result", business_profile)
